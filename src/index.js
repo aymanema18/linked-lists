77d835f6b2869da2f6715f1df8e5312521111376
aymanema18 <aymanema18@gmail.com>
2024-08-11 22:20:14 +0100

    Write linkedList, append and prepend functions

---
+++ src/index.js
@@ -0,0 +1,54 @@
+function linkedList() {
+    let node = nodeFactory();
+    function append(value) {
+        let temp = node;
+        while (temp.nextNode !== null) {
+            if (temp.value === null) {
+                temp.value = value;
+            }
+            temp = temp.nextNode;
+        }
+        if (temp.nextNode === null && temp.value === null) {
+            temp.value = value;
+        } else {
+            temp.nextNode = nodeFactory();
+            temp.nextNode.value = value;
+        }
+    }
+    function prepend(value) {
+        let temp = nodeFactory();
+        temp.value = value;
+        temp.nextNode = node;
+        node = temp;
+    }
+    function toString() {
+        let str = '';
+        let temp = node;
+        if (temp.value !== null) {
+            do {
+                if (str === '') {
+                    str += `( ${temp.value} )`;
+                } else {
+                    str += ` -> ( ${temp.value} )`;
+                }
+                temp = temp.nextNode;
+            } while (temp !== null);
+        }
+        console.log(str);
+    }
+    return { append, prepend, toString };
+}
+
+function nodeFactory() {
+    let value = null;
+    let nextNode = null;
+    return { value, nextNode };
+}
+
+let list = linkedList();
+list.append('Charlie');
+list.append('Karl');
+list.append('David');
+list.append('Simo');
+list.prepend('Aymane');
+list.toString();
